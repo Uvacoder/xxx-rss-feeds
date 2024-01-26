37dfa8e62f4403948430553738f97d4279cefe34
SJW <88535714+sarajw@users.noreply.github.com>
2024-01-26 21:36:16 +0100

    strip html before truncating

--- rss2html.js
+++ rss2html.js
@@ -18,7 +18,7 @@ async function rss2html(url, max) {
       link:
         item.querySelector("link")?.textContent ||
         item.querySelector("link")?.getAttribute("href"),
-      description: item.querySelector("description")?.textContent.substring(0,200)+"...",
+      description: item.querySelector("description")?.textContent.replace(/(<([^>]+)>)/gi, "").substring(0,150)+"...",
     }));
   console.info({ title, link, items, maxItems });
   return /*html*/ `<article>
